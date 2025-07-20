df17d06467ea8494bfffa246e94a08a6b9475621
Felix Knoerr <felix.knoerr@gmx.de>
2025-07-20 22:09:18 +0200

    basic functionality day switch

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import DaySwitch from "@/components/DaySwitch";
 import Timetable from "@/components/Timetable";
+import { $Enums } from "@/generated/prisma";
+import { useState } from "react";
 
 export default function TimetablePage() {
-  const day = "Fr";
+  // TODO get current day
+  const [day, setDay] = useState<$Enums.Day>("Fr");
 
+  // TODO get selectable days from DB
   return (
     <main className="flex flex-col items-center w-full">
       <div className="mb-6">
-        <DaySwitch />
+        <DaySwitch
+          day={day}
+          setDay={setDay}
+          selectableDays={["Fr", "Sa", "Su"]}
+        />
       </div>
       <div className="bg-amber-300 w-full flex flex-col items-center py-3">
         <Timetable />
--- src/components/DaySwitch.tsx
+++ src/components/DaySwitch.tsx
@@ -1,9 +1,53 @@
-import { FC } from "react";
+"use client";
 
-interface DaySwitchProps {}
+import type { $Enums, Day } from "@/generated/prisma";
+import { Dispatch, FC, SetStateAction, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/DropdownMenu";
+import { Button } from "./ui/Button";
 
-const DaySwitch: FC<DaySwitchProps> = ({}) => {
-  return <div>DaySwitch</div>;
+interface DaySwitchProps {
+  day: Day;
+  setDay: Dispatch<SetStateAction<$Enums.Day>>;
+  selectableDays?: Day[];
+}
+
+const DaySwitch: FC<DaySwitchProps> = ({ day, setDay, selectableDays }) => {
+  const dayOptions = selectableDays ?? [
+    "Mo",
+    "Tu",
+    "We",
+    "Th",
+    "Fr",
+    "Sa",
+    "Su",
+  ];
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="w-40">
+          {day}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {dayOptions.map((dayOption, index) => (
+          <DropdownMenuItem
+            key={index}
+            className="flex items-center justify-center"
+            onClick={() => setDay(dayOption)}
+          >
+            {dayOption === day && "> "}
+            {dayOption}
+            {dayOption === day && " <"}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default DaySwitch;
