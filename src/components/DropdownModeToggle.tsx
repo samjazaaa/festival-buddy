f0c7114e0b1596aa1175d424109c71c2c02d8ace
Felix Knoerr <felix.knoerr@gmx.de>
2025-07-21 11:29:43 +0200

    added option for system theme

--- src/components/DropdownModeToggle.tsx
+++ src/components/DropdownModeToggle.tsx
@@ -2,7 +2,7 @@
 
 import { FC } from "react";
 import { DropdownMenuItem } from "./ui/DropdownMenu";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Wallpaper } from "lucide-react";
 import { useTheme } from "next-themes";
 
 interface DropdownModeToggleProps {}
@@ -12,11 +12,21 @@ const DropdownModeToggle: FC<DropdownModeToggleProps> = ({}) => {
 
   return (
     <DropdownMenuItem
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      onClick={() =>
+        setTheme(
+          theme === "system" ? "dark" : theme === "dark" ? "light" : "system"
+        )
+      }
     >
+      {theme === "system" ? (
+        <Wallpaper className="mr-2 h-[1.2rem] w-[1.2rem]" />
+      ) : theme === "dark" ? (
+        <Moon className="mr-2 h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Sun className="mr-2 h-[1.2rem] w-[1.2rem]" />
+      )}
+
       <span>{theme}</span>
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
     </DropdownMenuItem>
   );
 };
